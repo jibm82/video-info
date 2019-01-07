c1d19169103da8b332f270a1db7321ab077bcbda
Jorge Baez <jibm_82@yahoo.com.mx>
2019-01-07 15:00:22 -0600

    Implemented initial main window

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const electron = require("electron");
-const { app } = electron;
+const { app, BrowserWindow } = electron;
 
 app.on("ready", () => {
-  console.log("App is now ready");
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
